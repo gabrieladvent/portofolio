588157762d10247f04d22b7087cc9810c9a23a3c
gabrieladvent <bie.ritan112@gmail.com>
2026-02-05 14:41:13 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-zinc-900">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind v4 works 🚀
+      </h1>
+    </div>
+  );
+}
